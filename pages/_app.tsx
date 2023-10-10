8c1a9cc76cd9fc5622ae55783bc912c6b82207f6
stanislaw-ch <post.chirko@gmail.com>
2023-10-10 22:39:54 +0200

    prepare to deploy: create empty project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import "../styles.css";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default App;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
---
+++ tina/config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "deploy";
+
+export default defineConfig({
+  branch,
+  clientId: "370a2c6a-5923-41cd-8639-e238b28e83db", // Get this from tina.io
+  token: "8a0a9be79a84c94dd66298be4e39fe019e086e11", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
