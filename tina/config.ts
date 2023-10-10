53ba3572f2b5b3c3057ffa466d0c9dcf72485b43
stanislaw-ch <post.chirko@gmail.com>
2023-10-10 22:50:24 +0200

    prepare to deploy: change branch name

--- tina/config.ts
+++ tina/config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "deploy";
+const branch = "deploy";
 
 export default defineConfig({
   branch,
-  clientId: "370a2c6a-5923-41cd-8639-e238b28e83db", // Get this from tina.io
-  token: "8a0a9be79a84c94dd66298be4e39fe019e086e11", // Get this from tina.io
+  clientId: "7cc81668-a861-493c-8565-7ff74727e636", // Get this from tina.io
+  token: "19150dc861c4d1cd320cce7ae79dd49a5e4b7a3d", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
