6e6ba9bf94deeeff30aa1f2c89e56e5d187601af
djoni2 <djoni2@users.noreply.github.com>
2023-12-29 18:12:35 +0000

    revert fix

--- components/layout/theme.tsx
+++ components/layout/theme.tsx
@@ -49,7 +49,14 @@ export const Theme = ({ data, children }) => {
 
   const { color = "blue", font = "sans", darkMode = "system" } = data;
 
-  React.useEffect(() => {
+ React.useEffect(() => {
+    updateRenderColorMode(
+      darkMode === "system"
+        ? systemDarkMode
+        : darkMode !== ""
+        ? darkMode
+        : "light"
+    );
     updateRenderColorMode("light");
   }, [systemDarkMode, darkMode]);
 
