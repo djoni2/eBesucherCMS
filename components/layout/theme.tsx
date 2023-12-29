c7792e2315143bdc797ff15cf5771e76b46afdbc
djoni2 <djoni@ebesucher.com>
2023-12-29 18:28:31 +0000

    Revert "revert fix"
    This reverts commit 6e6ba9bf94deeeff30aa1f2c89e56e5d187601af.

--- components/layout/theme.tsx
+++ components/layout/theme.tsx
@@ -49,14 +49,7 @@ export const Theme = ({ data, children }) => {
 
   const { color = "blue", font = "sans", darkMode = "system" } = data;
 
- React.useEffect(() => {
-    updateRenderColorMode(
-      darkMode === "system"
-        ? systemDarkMode
-        : darkMode !== ""
-        ? darkMode
-        : "light"
-    );
+  React.useEffect(() => {
     updateRenderColorMode("light");
   }, [systemDarkMode, darkMode]);
 
